932d11c58fbe426728c81f52ddef41426c91789b
TrungTLB <baotrung2852003@gmail.com>
2024-05-31 22:35:55 +0700

    config etherscan apikey

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
     solidity: "0.8.24",
@@ -11,6 +12,11 @@ const config: HardhatUserConfig = {
             chainId: 11155111,
             url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
             accounts: [PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
         },
     },
 };
